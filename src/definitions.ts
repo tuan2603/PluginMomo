24f8ab4c4fe048c596fadbc9dc43f1720d307a0e
Tuan Tran <tuantran@Tuans-MacBook-Pro.local>
2019-10-06 22:15:08 +0700

    a2

--- src/definitions.ts
+++ src/definitions.ts
@@ -6,5 +6,23 @@ declare module "@capacitor/core" {
 
 export interface MomoPluginPlugin {
   echo(options: { value: string }): Promise<{value: string}>;
-  payment(options: { value: string }): Promise<{value: string}>;
+  payment(options: { 
+    amount: number,
+    merchantName: string,
+    merchantCode: string,
+    orderId: string,
+    orderLabel: string,
+    merchantNameLabel: string,
+    total_fee: string,
+    description: string
+   }): Promise<{
+     amount : number,
+     merchantName: string,
+     merchantCode: string,
+     orderId: string,
+     orderLabel: string,
+     merchantNameLabel: string,
+     total_fee: string,
+     description: string
+    }>;
 }
